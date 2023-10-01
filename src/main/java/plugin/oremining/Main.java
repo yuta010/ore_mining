56f65932227180a613240da9216748ee91b108ff
yuta <kuromame.guri@gmail.com>
2023-10-01 23:57:46 +0900

    採掘した時点でスコアを加算する

--- src/main/java/plugin/oremining/Main.java
+++ src/main/java/plugin/oremining/Main.java
@@ -1,15 +1,15 @@
 package plugin.oremining;
 
 import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.oremining.command.OreMiningCommand;
 
-public final class Main extends JavaPlugin implements Listener {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
-        getCommand("oreMining").setExecutor(new OreMiningCommand());
+        OreMiningCommand oreMiningCommand = new OreMiningCommand();
+        Bukkit.getPluginManager().registerEvents(oreMiningCommand, this);
+        getCommand("oreMining").setExecutor(oreMiningCommand);
     }
 }
--- src/main/java/plugin/oremining/command/OreMiningCommand.java
+++ src/main/java/plugin/oremining/command/OreMiningCommand.java
@@ -5,14 +5,21 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
-public class OreMiningCommand implements CommandExecutor {
+public class OreMiningCommand implements CommandExecutor, Listener {
+
+  private Player player;
+  private int score;
 
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if(sender instanceof Player player){
+      this.player = player;
       player.sendMessage("鉱石採掘ゲームが開始されました！！");
 
       //ダイヤモンドのピッケルを取得
@@ -20,5 +27,16 @@ public class OreMiningCommand implements CommandExecutor {
       inventory.setItemInMainHand(new ItemStack(Material.DIAMOND_PICKAXE));
     }
     return false;
+  }
+
+  @EventHandler
+  public void PlayerHarvestBlockEvent(BlockBreakEvent e){
+    Player player = e.getPlayer();
+    if (this.player.equals(player)){
+      if(e.getBlock().getType().equals(Material.COAL_ORE)) {
+        score += 10;
+        player.sendMessage("プレイヤーは石炭をゲットしました！" + score + "点" );
+      }
+    }
   }
 }
