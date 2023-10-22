2b76571a4ce55857c744dc20a33d33c17ad77443
yuta <kuromame.guri@gmail.com>
2023-10-22 21:40:48 +0900

    Mybatisを使用してinsert処理を実行

--- src/main/java/plugin/oremining/mapper/PlayerScoreMapper.java
+++ src/main/java/plugin/oremining/mapper/PlayerScoreMapper.java
@@ -1,6 +1,7 @@
 package plugin.oremining.mapper;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import plugin.oremining.mapper.data.PlayerScore;
 
@@ -8,5 +9,7 @@ public interface PlayerScoreMapper {
 
   @Select("select * from player_score")
   List<PlayerScore> selectList();
+  @Insert("insert player_score(player_name, score, registered_at) values(#{playerName},#{score},now())")
+  int insert(PlayerScore playerScore);
 
 }
